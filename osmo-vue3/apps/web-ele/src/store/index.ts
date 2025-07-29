df6114b466c159e0cd7687dba99055089e2c9366
shenyuan <2497822530@qq.com>
2025-07-29 15:24:53 +0800

    feat: 提交

--- osmo-vue3/apps/web-ele/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './auth';
-
-export * from './gosmo/resource/agent';
-export * from './gosmo/analysis';
-export * from './gosmo/clean';
-export * from './gosmo/pool';
-export * from './gosmo/rule';
-export * from './gosmo/task';
